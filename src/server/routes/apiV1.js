ca1a338ae40af2735e23d31a7d8f40159cebe6ac
Dennis Bailey <bailey.dennis@gmail.com>
2016-05-24 09:31:02 -0600

    Added a route to GET all messages for one market

--- src/server/routes/apiV1.js
+++ src/server/routes/apiV1.js
@@ -2,6 +2,9 @@ var express = require('express');
 var api = require('../knex-queries/api-v1-queries');
 var router = express.Router();
 
+/*************************/
+/* --- MARKET ROUTES --- */
+/*************************/
 // Route to return a list of ALL Farmers Markets
 router.get('/markets', function(req, res, next) {
 
@@ -13,10 +16,29 @@ router.get('/markets', function(req, res, next) {
 
   .catch( function(error) { return res.status(401)
                                        .json({ status: 'There was an error',
-                                               data: error }); });
-
+                                               errorMsg: error }); });
 
 });
+
+
+/***************************/
+/* --- MESSAGES ROUTES --- */
+/***************************/
+// Route to return a list of ALL Messages for ONE Market
+router.get('/messages/:marketID', function(req, res, next) {
+
+  api.getMessages(req.params.marketID)
+
+  .then( function(result) { return res.status(200)
+                                      .json({ status: 'All the great Messages for Market ' + req.params.marketID,
+                                              data: result }); })
+
+  .catch( function(error) { return res.status(401)
+                                       .json({ status: 'There was an error',
+                                               errorMsg: error }); });
+
+});
+
 
 
 module.exports = router;
