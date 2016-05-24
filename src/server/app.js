deed2136495cb79a107b5ec563a2f28289b88e7d
Dennis Bailey <bailey.dennis@gmail.com>
2016-05-24 08:18:28 -0600

    Added a v1 route to app.js

--- src/server/app.js
+++ src/server/app.js
@@ -10,6 +10,7 @@ var swig = require('swig');
 
 // *** routes *** //
 var routes = require('./routes/index.js');
+var v1 = require('./routes/apiV1.js');
 
 
 // *** express instance *** //
@@ -36,6 +37,7 @@ app.use(express.static(path.join(__dirname, '../client')));
 
 // *** main routes *** //
 app.use('/', routes);
+app.use('/v1', v1);
 
 
 // catch 404 and forward to error handler
