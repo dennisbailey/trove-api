7ccefffbdb3cc5258048a49b921a3cb13fa34bbb
Dennis Bailey <bailey.dennis@gmail.com>
2016-05-24 09:30:23 -0600

    Added a knex query to return all the messages for one market

--- src/server/knex-queries/api-v1-queries.js
+++ src/server/knex-queries/api-v1-queries.js
@@ -2,6 +2,17 @@ var knex = require('../../../db/knex');
 
 module.exports = {
   
-  getAll: function (table) { return knex(table); }
+  // *** Flexible Queries *** //
+  
+  getAll: function (table) { return knex(table); },
+  
+  
+  // *** /markets Queries *** //
+  
+  
+  // *** /messages Queries *** //
+  getMessages: function(marketID) { return knex('messages')
+                                           .where('market_id', marketID)
+                                           .orderBy('dt', 'desc') }
 
 };
