4fdcf18e94f73f8127c0f1e14b46ee07092a2855
Dennis Bailey <bailey.dennis@gmail.com>
2016-05-24 09:29:24 -0600

    Tweaked the default language for index.html from Express to Trove

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Welcome to the Treasure Trove API' });
 });
 
 module.exports = router;
