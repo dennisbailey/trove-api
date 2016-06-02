f53e85ade274fe2741d64b4aa9c55390616fc5f7
Kathy Dieser <katdiese@gmail.com>
2016-06-02 10:31:59 -0600

    troubleshooting CORS

--- src/server/app.js
+++ src/server/app.js
@@ -31,7 +31,7 @@ app.set('views', path.join(__dirname, 'views'));
 //*** allows CORS ***//
 app.use(function(req, res, next) { //allow cross origin requests
     res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
-    res.header("Access-Control-Allow-Origin", "http://localhost:8888");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
