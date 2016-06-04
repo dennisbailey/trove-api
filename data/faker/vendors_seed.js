ce94a3edba08ef5e97bbcbc7fe78e81c7cc8bcec
Dennis Bailey <bailey.dennis@gmail.com>
2016-06-04 17:57:55 -0600

    WIP vendor table and markets_vendors seed data

---
+++ data/faker/vendors_seed.js
@@ -0,0 +1,21 @@
+var faker = require('faker');
+
+category_counts = [2181, 4631, 2677, 3194, 3661, 3853, 1280, 4196, 5142, 4277, 4242, 1653, 2866, 406, 1505, 3596, 2307, 3185, 3475, 1551, 873, 1547, 639, 4097, 651, 1022, 1007, 816, 167, 586];
+
+console.log('vendor_name,category_id');
+
+// For all 8557 markets, create one vendor per applicable category
+for (var i = 1; i < category_counts.length ; i++) { 
+  
+  for (var j = 0; j < category_counts[i] ; j++) { 
+    
+    var vendorName = faker.company.companyName();
+    var category_id = j + 1;
+    
+    console.log( vendorName + ',' + category_id );
+        
+  }
+  
+}
+
+// faker.company.companyName will produce names with a comma in them. Replace all the ', ' with ' ' to create a valid .csv
---
+++ data/markets_vendors_seed.js
@@ -0,0 +1,5 @@
+console.log('vendor_id');
+
+for (var i = 1; i < 71284; i++) { 
+  console.log(i);
+}
